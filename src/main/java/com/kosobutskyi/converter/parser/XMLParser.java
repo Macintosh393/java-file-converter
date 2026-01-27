b31807f3dc3f6335520118521a421e5ab5bfe945
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2026-01-27 21:36:02 +0200

    Fix parsed XML node for generators

--- src/main/java/com/kosobutskyi/converter/parser/XMLParser.java
+++ src/main/java/com/kosobutskyi/converter/parser/XMLParser.java
@@ -1,6 +1,7 @@
 package com.kosobutskyi.converter.parser;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.kosobutskyi.converter.dto.ParsedDTO;
 
@@ -13,9 +14,19 @@ public class XMLParser extends FileParser {
         try {
             XmlMapper xmlMapper = new XmlMapper();
             JsonNode rootNode = xmlMapper.readTree(inputFile);
+            rootNode = parseArray(rootNode);
             return new ParsedDTO(rootNode);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static JsonNode parseArray(JsonNode parentNode) {
+        if (!parentNode.isArray()) {
+            for (JsonNode child : parentNode) {
+                parentNode = parseArray(child);
+            }
+        }
+        return parentNode;
     }
 }
